96737b40c36cdb9acc7a6aa5f48ddcdf20f3c81a
Luis Cesar Maiaru <luiscesar.mayaru@gmail.com>
2022-10-18 08:52:03 +0100

    MyBTreeSet, from function added. Tests realized.

--- common/src/collections/set/mybtreeset/mod.rs
+++ common/src/collections/set/mybtreeset/mod.rs
@@ -9,10 +9,24 @@ where
     values: BTreeSet<T>,
 }
 
-impl<T: Eq> MyBTreeSet<T> {
-    pub fn from(vals: BTreeSet<T>) -> MyBTreeSet<T> {
-        let mut myset = MyBTreeSet { values: vals };
-        myset
+impl<T:Eq> MyBTreeSet<T> {
+    pub fn new() -> MyBTreeSet<T> {
+        MyBTreeSet { values: BTreeSet::new() }
+    }
+}
+
+impl<T:Ord, const N: usize> From<[T; N]> for MyBTreeSet<T> {
+    fn from(arr: [T; N]) -> Self {
+        if N == 0 {
+            return MyBTreeSet{values:BTreeSet::new()}
+        }
+        let mut my_set = MyBTreeSet::new();
+        for x in arr {
+            my_set.add(x);
+        }
+        my_set
+        //let values = BTreeSet::from(arr);
+        //MyBTreeSet { values }
     }
 }
 
@@ -38,7 +52,7 @@ impl<T: Eq + Ord> MySet<T> for MyBTreeSet<T> {
     }
 
     fn nil() -> Self {
-        MyBTreeSet::from(BTreeSet::new())
+        MyBTreeSet::new()
     }
 }
 
@@ -55,43 +69,35 @@ mod tests {
 
     #[test]
     fn test_mybtreeset_new_case1() {
-        println!("test_mybtreeset_new_case1: begin");
-        let mut v: BTreeSet<String> = BTreeSet::new();
-        let mut v2: BTreeSet<String> = BTreeSet::new();
-        let myset = MyBTreeSet::from(v);
-        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: v2 };
-        println!("myset {:?}", myset);
+        let myset = MyBTreeSet::new();
+        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: BTreeSet::new() };
         assert_eq!(myset, expected_myset)
     }
 
     #[test]
     fn test_mybtreeset_nil_case1() {
-        let mut v2: BTreeSet<String> = BTreeSet::new();
         let myset = MyBTreeSet::nil();
-        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: v2 };
+        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: BTreeSet::new() };
         println!("myset {:?}", myset);
         assert_eq!(myset, expected_myset)
     }
 
     #[test]
     fn test_mybtreeset_add_case1() {
-        let mut v: BTreeSet<String> = BTreeSet::new();
-        let mut v2: BTreeSet<String> = BTreeSet::new();
-        v2.insert(String::from("Hello"));
-        let mut myset = MyBTreeSet::from(v);
-        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: v2 };
+        let mut myset = MyBTreeSet::new();
+        let expected_myset: MyBTreeSet<String> = MyBTreeSet::from(["Hello".to_string()]);
         myset.add(String::from("Hello"));
         println!("expected_myset {:?}", expected_myset);
         println!("myset {:?}", myset);
-        assert_eq!(myset, expected_myset);
+        assert_eq!(expected_myset, myset);
     }
 
     #[test]
     fn test_mybtreeset_remove_case1() {
-        let v: BTreeSet<String> = BTreeSet::from([String::from("Hello")]);
-        let v2: BTreeSet<String> = BTreeSet::from([String::from("Hello")]);
-        let mut myset = MyBTreeSet::from(v);
-        let expected_myset: MyBTreeSet<String> = MyBTreeSet { values: v2 };
+        let mut myset = 
+            MyBTreeSet::from([String::from("Hello")]);
+        let expected_myset: MyBTreeSet<String> = 
+            MyBTreeSet { values: BTreeSet::from([String::from("Hello")]) };
         myset.add(String::from("Hello2"));
         myset.remove(&String::from("Hello2"));
         assert_eq!(myset, expected_myset);
@@ -99,22 +105,20 @@ mod tests {
 
     #[test]
     fn test_mybtreeset_size_case1() {
-        let v: BTreeSet<String> = BTreeSet::from([String::from("He"), String::from("llo")]);
-        let myset = MyBTreeSet::from(v);
+        let myset:MyBTreeSet<String> = 
+            MyBTreeSet::from([String::from("He"), String::from("llo")]);
         assert_eq!(myset.size(), 2);
     }
 
     #[test]
     fn test_mybtreeset_is_empty_case1() {
-        let mut v: BTreeSet<String> = BTreeSet::new();
-        let myset = MyBTreeSet::from(v);
+        let myset:MyBTreeSet<String> = MyBTreeSet::from([]);
         assert!(myset.is_empty());
     }
 
     #[test]
     fn test_mybtreeset_is_empty_case2() {
-        let mut v: BTreeSet<String> = BTreeSet::from([String::from("Hello")]);
-        let myset = MyBTreeSet::from(v);
+        let myset:MyBTreeSet<String>= MyBTreeSet::from([String::from("Hello")]);
         assert!(!myset.is_empty());
     }
 }
